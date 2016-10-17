dfdcb399c51d4e785b693b8a133f660604992932
Steven Than <steventhan11@gmail.com>
2016-10-17 13:27:09 -0700

    done extract 1 restaurant div

---
+++ scraper.py
@@ -0,0 +1,59 @@
+import requests
+import sys
+import re
+from bs4 import BeautifulSoup
+
+DOMAIN = 'http://info.kingcounty.gov'
+PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+PARAMS = {
+    'Output': 'W',
+    'Business_Name': '',
+    'Business_Address': '',
+    'Longitude': '',
+    'Latitude': '',
+    'City': '',
+    'Zip_Code': '',
+    'Inspection_Type': 'All',
+    'Inspection_Start': '',
+    'Inspection_End': '',
+    'Inspection_Closed_Business': 'A',
+    'Violation_Points': '',
+    'Violation_Red_Points': '',
+    'Violation_Descr': '',
+    'Fuzzy_Search': 'N',
+    'Sort': 'H',
+}
+
+
+def get_inspection_page(**kwargs):
+    url = DOMAIN + PATH
+    params = PARAMS.copy()
+    for k, v in kwargs.items():
+        if k in params:
+            params[k] = v
+    res = requests.get(url, params=params)
+    return res.content, res.encoding
+
+
+def parse_source(html, encoding='utf-8'):
+    return BeautifulSoup(html, 'html5lib', from_encoding=encoding)
+
+
+def extract_data_listings(html):
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+if __name__ == '__main__':
+    kwargs = {
+        'Inspection_Start': '10/1/2015',
+        'Inspection_End': '2/1/2016',
+        'Zip_Code': '98122'
+    }
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html, encoding = load_inspection_page('inspection_page.html')
+    else:
+        html, encoding = get_inspection_page(**kwargs)
+    doc = parse_source(html, encoding)
+    listings = extract_data_listings(doc)
+    print(len(listings))
+    print(listings[0].prettify())
